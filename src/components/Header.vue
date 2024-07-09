2ea39e0a534802c1692070e67a0baa4cf2bb7dff
fraberret <f.berretta02@gmail.com>
2024-07-09 16:52:48 +0200

    add color palette

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,13 @@
     export default {
         name:'Header'
     }
-    
+
 </script>
 
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <header>
+        
+    </header>
 </template>
 
 
