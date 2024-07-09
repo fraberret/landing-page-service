045953056298ab43d13bb6484cf55fe458266afa
fraberret <f.berretta02@gmail.com>
2024-07-09 17:06:54 +0200

    import image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,12 +7,30 @@
 
 <template>
     <header>
-        
+        <div class="header-banner text-center py-2">
+            <p class="m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, hic?</p>
+        </div>
+
+           <nav class="navbar navbar-expand-sm navbar-light bg-primary">
+               <div class="container-fluid">
+                   <a class="navbar-brand" href="#"><img src="" alt=""></a>
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                       aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                   </button>
+                   <div class="collapse navbar-collapse" id="navbarID">
+                       <div class="navbar-nav">
+                           <a class="nav-link active" aria-current="page" href="#">Home</a>
+                           
+                       </div>
+                   </div>
+               </div>
+           </nav>
     </header>
 </template>
 
 
 
-<style lang="scss" scoped>
+<style >
 
 </style>
